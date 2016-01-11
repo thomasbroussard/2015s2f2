7e7014d23fab81bfc7788a1ce0854131865393d0
Thomas Broussard <thomas.broussard@gmail.com>
2016-01-11 19:53:16 +0100

    *upd : refactor to optimize search method

--- IamCore/test/fr/tbr/iamcore/tests/services/dao/xml/IdentityXmlDAO.java
+++ IamCore/test/fr/tbr/iamcore/tests/services/dao/xml/IdentityXmlDAO.java
@@ -20,13 +20,12 @@ import fr.tbr.iamcore.tests.services.match.Matcher;
 import fr.tbr.iamcore.tests.services.match.impl.StartsWithIdentityMatchStrategy;
 
 public class IdentityXmlDAO implements IdentityDAO {
-	
-	Matcher<Identity> matcher = new StartsWithIdentityMatchStrategy();
-	
 
-	@Override
-	public List<Identity> search(Identity criteria) {
-		List<Identity> results = new ArrayList<Identity>();
+	Matcher<Identity> matcher = new StartsWithIdentityMatchStrategy();
+
+	Document doc;
+
+	public IdentityXmlDAO() {
 		try {
 			// Intermediate instance to build a document builder (the factory to
 			// parse DOM documents)
@@ -35,81 +34,124 @@ public class IdentityXmlDAO implements IdentityDAO {
 			// an xml source
 			DocumentBuilder db = dbf.newDocumentBuilder();
 			// Document representation in Java
-			Document doc = db.parse("F:\\Work\\Dev\\Git\\Epita\\Fundamentals\\2015s2f2\\IamCore\\xml\\identities.xml");
-	
-			// gets all the nodes called "identity" (see xml/identities.xml in
-			// the project)
-			NodeList nodes = doc.getElementsByTagName("identity");
-			int nodesSize = nodes.getLength();
-
-			// for every found identity
-			for (int i = 0; i < nodesSize; i++) {
-				Node node = nodes.item(i);
-				// test if the found node is really an Element
-				// in the DOM implementation a Node can represent an Element, an
-				// Attribute or a TextContent
-				// so we have to be sure that the found node is of type
-				// "Element" using the instanceof operator
-				if (node instanceof Element) {
-
-					// cast the node into an Element, as we are sure it is an
-					// instance of Element
-					Element identity = (Element) node;
-
-					// get the properties for the encountered identity
-					NodeList properties = identity.getElementsByTagName("property");
-					int length = properties.getLength();
-
-					// declare and initialize several variables on the same line
-					String displayName = "", guid = "", email = "";
-					Date birthDate = null;
-					// for every found property
-					for (int j = 0; j < length; j++) {
-						Node item = properties.item(j);
-						// we check that the found property is really an element
-						// (see the explanation above)
-						if (item instanceof Element) {
-							Element propertyElt = (Element) item;
-							// we need to store the right value in the right
-							// property so we use a switch-case structure
-							// to handle the 3 cases
-							String textContent = propertyElt.getTextContent();
-							switch (propertyElt.getAttribute("name")) {
-							case "displayName":
-								displayName = textContent;
-								break;
-							case "guid":
-								guid = textContent;
-								break;
-							case "email":
-								email = textContent;
-								break;
-								
-							case "birthDate":
-								birthDate = new SimpleDateFormat("dd/MM/yyyy").parse(textContent);
-								break;
-								
-							default:
-								// the encountered property name is not expected
-								// so we use the "default" case
-								break;
-							}
-						}
-					}
-					Identity currentIdentity = new Identity(displayName, email, guid);
-					currentIdentity.setBirthDate(birthDate);
-					//usage of Matcher to filter only the wished identities.
-					if (this.matcher.match(criteria, currentIdentity)){
-						results.add(currentIdentity);
-					}
-
-				}
-			}
+			this.doc = db.parse("F:\\Work\\Dev\\Git\\Epita\\Fundamentals\\2015s2f2\\IamCore\\xml\\identities.xml");
 		} catch (Exception e) {
 			e.printStackTrace();
+			// TODO complete exception handling
+		}
+	}
+
+	@Override
+	public List<Identity> search(Identity criteria) {
+		List<Identity> results = new ArrayList<Identity>();
+		// gets all the nodes called "identity" (see xml/identities.xml in
+		// the project)
+		NodeList nodes = this.doc.getElementsByTagName("identity");
+		int nodesSize = nodes.getLength();
+
+		// for every found identity
+		for (int i = 0; i < nodesSize; i++) {
+			Node node = nodes.item(i);
+			// test if the found node is really an Element
+			// in the DOM implementation a Node can represent an Element, an
+			// Attribute or a TextContent
+			// so we have to be sure that the found node is of type
+			// "Element" using the instanceof operator
+			if (node instanceof Element) {
+
+				Identity identity = readOneIdentityFromXmlElement(node);
+				// usage of Matcher to filter only the wished identities.
+				if (this.matcher.match(criteria, identity)) {
+					results.add(identity);
+				}
+			}
 		}
 
 		return results;
+	}
+
+	
+	public List<Identity> readAll(){
+		List<Identity> results = new ArrayList<Identity>();
+		// gets all the nodes called "identity" (see xml/identities.xml in
+		// the project)
+		NodeList nodes = this.doc.getElementsByTagName("identity");
+		int nodesSize = nodes.getLength();
+
+		// for every found identity
+		for (int i = 0; i < nodesSize; i++) {
+			Node node = nodes.item(i);
+			// test if the found node is really an Element
+			// in the DOM implementation a Node can represent an Element, an
+			// Attribute or a TextContent
+			// so we have to be sure that the found node is of type
+			// "Element" using the instanceof operator
+			if (node instanceof Element) {
+
+				Identity identity = readOneIdentityFromXmlElement(node);
+				// usage of Matcher to filter only the wished identities.
+				
+				results.add(identity);
+			
+			}
+		}
+
+		return results;
+		
+	}
+	private Identity readOneIdentityFromXmlElement(Node node) {
+		// cast the node into an Element, as we are sure it is an
+		// instance of Element
+		Element identity = (Element) node;
+
+		// get the properties for the encountered identity
+		NodeList properties = identity.getElementsByTagName("property");
+		int length = properties.getLength();
+
+		// declare and initialize several variables on the same line
+		String displayName = "", guid = "", email = "";
+		Date birthDate = null;
+		// for every found property
+		for (int j = 0; j < length; j++) {
+			Node item = properties.item(j);
+			// we check that the found property is really an element
+			// (see the explanation above)
+			if (item instanceof Element) {
+				Element propertyElt = (Element) item;
+				// we need to store the right value in the right
+				// property so we use a switch-case structure
+				// to handle the 3 cases
+				String textContent = propertyElt.getTextContent();
+				switch (propertyElt.getAttribute("name")) {
+				case "displayName":
+					displayName = textContent;
+					break;
+				case "guid":
+					guid = textContent;
+					break;
+				case "email":
+					email = textContent;
+					break;
+
+				case "birthDate":
+					try {
+						birthDate = new SimpleDateFormat("dd/MM/yyyy").parse(textContent);
+					} catch (Exception e) {
+						e.printStackTrace();
+						//TODO finish exception handling
+					}
+					break;
+
+				default:
+					// the encountered property name is not expected
+					// so we use the "default" case
+					break;
+				}
+			}
+		}
+		Identity currentIdentity = new Identity(displayName, email, guid);
+		currentIdentity.setBirthDate(birthDate);
+		return currentIdentity;
 	}
 
 	@Override
